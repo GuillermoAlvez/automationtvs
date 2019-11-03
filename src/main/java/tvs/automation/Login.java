95ddf4890f738b36d985de5be23ef85519991698
jalvezCasa <jalvez@casa>
2019-11-03 13:55:20 -0300

    initial commit

---
+++ src/main/java/tvs/automation/Login.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tvs.automation;
+
+/**
+ *
+ * @author jalvez
+ */
+public class Login {
+
+    public static void acceder() {
+        System.out.println("accede");
+    }
+
+    public static void salir() {
+        System.out.println("sale");
+    }
+
+}
